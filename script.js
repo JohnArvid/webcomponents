ec5784f924cfe5c32bc8b9efcfe6249417456433
JohnArvid <arvid-lindstrom@live.se>
2025-08-13 19:33:54 +0200

    Skapa egen webcomponent som visar tiden och uppdateras varje sekund, testa shadowDOM

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // create a class that extends HTMLElement
 class MyComponent extends HTMLElement {
+	
 	// component implementation
 	// lifecycle callbacks: 
 	connectedCallback() {
@@ -38,3 +39,23 @@ class CurrentDate extends HTMLElement {
 
 // Register the CurrentDate component using the tag name <current-date>.
 customElements.define('current-date', CurrentDate);
+
+
+
+class CurrentTime extends HTMLElement {
+    // The browser calls this method when the element is
+    // added to the DOM.
+    connectedCallback() {
+        // Create a Date object representing the current date.
+        const tick = setInterval(()=>{
+			let now = new Date();
+
+        	// Format the date to a human-friendly string, and set the
+			// formatted date as the text content of this element.
+        	this.textContent = now.toLocaleTimeString();
+		}, 1000 )
+    }
+}
+
+// Register the CurrentDate component using the tag name <current-date>.
+customElements.define('current-time', CurrentTime);
